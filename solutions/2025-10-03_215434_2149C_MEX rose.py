3813e6870569b10202c4923e465aebab28f4849f
Ryan Netto <ryan.netto@btech.christuniversity.in>
2025-10-03 21:54:34 +0530

    Add 2149C - MEX rose

---
+++ solutions/2025-10-03_215434_2149C_MEX rose.py
@@ -0,0 +1,6 @@
+# Problem: https://codeforces.com/contest/2149/problem/C
+# Submission: https://codeforces.com/contest/2149/submission/341712127
+# Language: Python 3
+# Solved at: 2025-10-03 21:54:34
+
+# (Source file not found locally)
