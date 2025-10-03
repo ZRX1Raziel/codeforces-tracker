65a1a6215a679c00c058a48c453c2761cac9bba7
Ryan Netto <ryan.netto@btech.christuniversity.in>
2025-10-03 21:54:05 +0530

    Add 1409A - Yet Another Two Integers Problem

---
+++ solutions/2025-10-03_215405_1409A_Yet Another Two Integers Problem.py
@@ -0,0 +1,6 @@
+# Problem: https://codeforces.com/contest/1409/problem/A
+# Submission: https://codeforces.com/contest/1409/submission/339933572
+# Language: Python 3
+# Solved at: 2025-10-03 21:54:05
+
+# (Source file not found locally)
