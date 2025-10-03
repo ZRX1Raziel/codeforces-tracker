7ac5ce399ab3813dd1bcd90e6e0c09b6c24cb65a
Ryan Netto <ryan.netto@btech.christuniversity.in>
2025-10-03 21:54:29 +0530

    Add 2149A - Be Positive

---
+++ solutions/2025-10-03_215428_2149A_Be Positive.py
@@ -0,0 +1,6 @@
+# Problem: https://codeforces.com/contest/2149/problem/A
+# Submission: https://codeforces.com/contest/2149/submission/341652098
+# Language: Python 3
+# Solved at: 2025-10-03 21:54:28
+
+# (Source file not found locally)
