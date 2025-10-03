8d8dc381406ed04f9f16cd7bce52a75f7ef6d01a
Ryan Netto <ryan.netto@btech.christuniversity.in>
2025-10-03 21:54:31 +0530

    Add 2149B - Unconventional Pairs

---
+++ solutions/2025-10-03_215431_2149B_Unconventional Pairs.py
@@ -0,0 +1,6 @@
+# Problem: https://codeforces.com/contest/2149/problem/B
+# Submission: https://codeforces.com/contest/2149/submission/341657989
+# Language: Python 3
+# Solved at: 2025-10-03 21:54:31
+
+# (Source file not found locally)
