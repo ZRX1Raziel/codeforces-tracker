40a2f0dfb3693939f2c711af4934d5910c4c8c63
Ryan Netto <ryan.netto@btech.christuniversity.in>
2025-10-27 20:55:28 +0530

    Add 4C - Registration System

---
+++ solutions/2025-10-27_205528_4C_Registration System.py
@@ -0,0 +1,25 @@
+# Problem: https://codeforces.com/contest/4/problem/C
+# Submission: https://codeforces.com/contest/4/submission/345187459
+# Language: C++20 (GCC 13-64)
+# Solved at: 2025-10-27 20:55:28
+
+t=int(input())
+
+for _ in range(t):
+
+    n=int(input())
+    s=input()
+    lower=0
+    lowest=0
+
+    for i in range(n):
+        if s[i]==')':
+            lower-=1
+        else:
+            lower+=1
+        
+        lowest=min(lower,lowest)
+
+    print(abs(lowest))
+
+
