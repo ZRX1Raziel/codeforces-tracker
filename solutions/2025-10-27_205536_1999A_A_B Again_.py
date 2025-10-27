c22595175b09f386c87b3186d587714b09252adf
Ryan Netto <ryan.netto@btech.christuniversity.in>
2025-10-27 20:55:36 +0530

    Add 1999A - A+B Again?

---
+++ solutions/2025-10-27_205536_1999A_A_B Again_.py
@@ -0,0 +1,11 @@
+# Problem: https://codeforces.com/contest/1999/problem/A
+# Submission: https://codeforces.com/contest/1999/submission/345209939
+# Language: PyPy 3-64
+# Solved at: 2025-10-27 20:55:36
+
+t=int(input())
+
+for _ in range(t):
+    n=input()
+    sum=int(n[0])+int(n[1])
+    print(sum)
