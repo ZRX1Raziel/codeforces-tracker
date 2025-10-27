b43f865f733fd849b44cad0010954c9319b5438d
Ryan Netto <ryan.netto@btech.christuniversity.in>
2025-10-27 20:55:04 +0530

    Add 1834A - Unit Array

---
+++ solutions/2025-10-27_205503_1834A_Unit Array.py
@@ -0,0 +1,25 @@
+# Problem: https://codeforces.com/contest/1834/problem/A
+# Submission: https://codeforces.com/contest/1834/submission/343954911
+# Language: Python 3
+# Solved at: 2025-10-27 20:55:03
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    neg = a.count(-1)
+    pos = n - neg
+    ops = 0
+
+    # Make sure sum >= 0
+    while pos < neg:
+        neg -= 1
+        pos += 1
+        ops += 1
+
+    # Make sure product = 1 (even number of -1s)
+    if neg % 2 == 1:
+        ops += 1
+
+    print(ops)
