567846b3737b049d4f243ed58f5d704789057eec
Ryan Netto <ryan.netto@btech.christuniversity.in>
2025-11-29 23:09:13 +0530

    Add 2171C2 - Renako Amaori and XOR Game (hard version)

---
+++ solutions/2025-11-29_230913_2171C2_Renako Amaori and XOR Game _hard version_.cpp
@@ -0,0 +1,6 @@
+# Problem: https://codeforces.com/contest/2171/problem/C2
+# Submission: https://codeforces.com/contest/2171/submission/350401225
+# Language: C++20 (GCC 13-64)
+# Solved at: 2025-11-29 23:09:13
+
+# (Source file not found locally)
