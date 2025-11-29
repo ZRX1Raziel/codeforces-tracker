337bfcf358612d9fefc4196cbc945307c00a7227
Ryan Netto <ryan.netto@btech.christuniversity.in>
2025-11-29 23:08:53 +0530

    Add 2065C1 - Skibidus and Fanum Tax (easy version)

---
+++ solutions/2025-11-29_230853_2065C1_Skibidus and Fanum Tax _easy version_.cpp
@@ -0,0 +1,6 @@
+# Problem: https://codeforces.com/contest/2065/problem/C1
+# Submission: https://codeforces.com/contest/2065/submission/349601755
+# Language: C++20 (GCC 13-64)
+# Solved at: 2025-11-29 23:08:53
+
+# (Source file not found locally)
