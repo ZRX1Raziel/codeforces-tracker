0b2e972d35e672d4cda5f62282b5ff1ec37b22a1
Ryan Netto <ryan.netto@btech.christuniversity.in>
2026-01-15 19:02:42 +0530

    Add 1352B - Same Parity Summands

---
+++ solutions/2026-01-15_190242_1352B_Same Parity Summands.cpp
@@ -0,0 +1,32 @@
+# Problem: https://codeforces.com/contest/1352/problem/B
+# Submission: https://codeforces.com/contest/1352/submission/357246815
+# Language: C++20 (GCC 13-64)
+# Solved at: 2026-01-15 19:02:42
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+		int n1 = n - (k - 1);
+		if (n1 > 0 && n1 % 2 == 1) {
+			cout << "YES" << endl;
+			for (int i = 0; i < k - 1; ++i) cout << "1 ";
+			cout << n1 << endl;
+			continue;
+		}
+		int n2 = n - 2 * (k - 1);
+		if (n2 > 0 && n2 % 2 == 0) {
+			cout << "YES" << endl;
+			for (int i = 0; i < k - 1; ++i) cout << "2 ";
+			cout << n2 << endl;
+			continue;
+		}
+		cout << "NO" << endl;
+	}
+}
