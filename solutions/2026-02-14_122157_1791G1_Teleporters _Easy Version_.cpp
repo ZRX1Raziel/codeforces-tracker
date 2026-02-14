a7ab98053acffce0a2ea9a73fc36049e38f55204
Ryan Netto <ryan.netto@btech.christuniversity.in>
2026-02-14 12:21:58 +0530

    Add 1791G1 - Teleporters (Easy Version)

---
+++ solutions/2026-02-14_122157_1791G1_Teleporters _Easy Version_.cpp
@@ -0,0 +1,6 @@
+# Problem: https://codeforces.com/contest/1791/problem/G1
+# Submission: https://codeforces.com/contest/1791/submission/362797626
+# Language: C++20 (GCC 13-64)
+# Solved at: 2026-02-14 12:21:57
+
+# (Source file not found locally)
